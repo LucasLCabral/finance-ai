0a6d78cc99d3c379c689e4eae5834797db7c251d
Lucas <lucasludovicocabraal@gmail.com>
2025-04-21 10:29:54 -0300

    feat: ensure user is redirected to stripe checkout when acquiring plan

---
+++ app/subscription/_actions/create-stripe-checkout/index.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import Stripe from "stripe";
+
+export const createStripeCheckout = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+  if (!process.env.STRIPE_SECRET_KEY_ID) {
+    throw new Error("Stripe secret key not set");
+  }
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY_ID, {
+    apiVersion: "2025-03-31.basil",
+  });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "subscription",
+    success_url: "http://localhost:3000/",
+    cancel_url: "http://localhost:3000/",
+    subscription_data: {
+      metadata: {
+        clerk_user_id: userId,
+      },
+    },
+    line_items: [
+      {
+        price: process.env.STRIPE_PREMIUM_PLAN_PRICE_ID,
+        quantity: 1,
+      },
+    ],
+  });
+  return { sessionId: session.id };
+};
