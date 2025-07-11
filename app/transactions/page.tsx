0777fb6e6c69d110a2376cdb18434979ede8d404
Lucas <lucasludovicocabraal@gmail.com>
2025-07-10 22:08:20 -0300

    refactor: ensure Decimal warning does not happen

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -20,6 +20,9 @@ const TransactionsPage = async () => {
     where: {
       userId,
     },
+    orderBy: {
+      date: "desc",
+    },
   });
 
   const userCanAddTransaction = await canUserAddTransaction();
@@ -32,7 +35,10 @@ const TransactionsPage = async () => {
           <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
         <ScrollArea className="h-[80vh]">
-          <DataTable columns={transactionColumns} data={transactions} />
+          <DataTable
+            columns={transactionColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
         </ScrollArea>
       </div>
     </>
